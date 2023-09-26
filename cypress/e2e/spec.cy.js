9c89ab10ac91da10a6acd8c48219ca7f66b82d6d
pramia <pedroramia@hotmail.com>
2023-09-26 12:28:30 +0200

    set baseUrl & first test check for localhost

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
-  });
-  it('test 2', () => {
-    cy.visit('https://example.cypress.io');
+describe('List of Employees Table', () => {
+  it('visit employees endpoint', () => {
+    cy.visit('/employees.html');
     cy.log('all fine');
-  })
-})
+  });
+});
